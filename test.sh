d754ea268d412b39f4a6629f0cc667b122314fb6
provrb <ethanjamesoliveira@gmail.com>
2025-01-15 20:10:40 -0500

    push updates

--- test.sh
+++ test.sh
@@ -1,13 +1,15 @@
 # dir names
+TEST_DIR="tests"
 SRC_DIR="src"
 OUT_DIR="out"
 API_DIR="$SRC_DIR/api"
 
 # input and output files
-OUT="$OUT_DIR/main.exe"
-SRC="$SRC_DIR/*.c $API_DIR/*.c"
+OUT="$OUT_DIR/tests.exe"
+SRC="$TEST_DIR/tests.c $API_DIR/*.c"
 
 clear # clear output
 rm -f "$OUT"
 gcc $SRC -o "$OUT" -Wall -Wextra -Werror
+chmod +x "$OUT"
 ./"$OUT"
--- tests/tests.c
+++ tests/tests.c
@@ -1,1 +1,18 @@
-#include 
+#include <stdio.h>
+#include <assert.h>
+
+#include "src/api/macros.h"
+
+Instruction program[] = {    
+    INST_PUSH(4),
+    INST_PUSH(3),
+    INST_ORB(),
+    INST_PRNT(),
+};
+
+#include "api/inst.h"
+
+int main() {
+    printf("hello");
+    return 0;
+}
